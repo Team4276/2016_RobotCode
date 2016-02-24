9bc592222af471b4e92e9c259d8fe5b2e170d9e7
Austin Hwang <austin.y.hwang@gmail.com>
2016-02-23 17:55:40 -0800

    Update from the team laptop used to load Geir  robot 23-Feb

---
+++ src/org/usfirst/frc/team4276/robot/DashboardOutput.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team4276.robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DashboardOutput extends Thread implements Runnable{
+	
+	public DashboardOutput()
+	{
+		
+	}
+	
+	
+	
+	public void run()
+	{
+		while(true)
+		{
+			Timer.delay(.005);
+			SmartDashboard.putNumber("X", IMU.imu.getAccelX());
+        	SmartDashboard.putNumber("Y", IMU.imu.getAccelY());
+        	SmartDashboard.putNumber("Z", IMU.imu.getAccelZ());
+        	SmartDashboard.putNumber("Roll: ", IMU.imu.getRoll());
+        	SmartDashboard.putNumber("Pitch: ", IMU.imu.getPitch());
+        	SmartDashboard.putNumber("Yaw: ", IMU.imu.getYaw());
+        	SmartDashboard.putNumber("Yaw Rate: ", IMU.imu.getRateZ());
+        	
+        	SmartDashboard.putNumber("Encoder Value: ", Arm.enc.getDistance());
+        	SmartDashboard.putBoolean("Switch", !ArmPID.SET_90_OPTICAL.get());
+        	SmartDashboard.putNumber("Arm Angle: ", ArmPID.angle);
+        	SmartDashboard.putNumber("Arm Setpoint: ", ArmPID.setpoint);
+        	
+        	SmartDashboard.putBoolean("Intake Limit Switch: ", Arm.limiter.get());
+        	SmartDashboard.putBoolean("Auto Shoot In Progress: ", Arm.autoRun_In_Progress);
+        	
+        	SmartDashboard.putNumber("Drive Encoder: ", TankDrive.driveenc.getDistance());
+        	
+        	SmartDashboard.putNumber("Shooter Encoder: ", Shooter.shooterenc.getRate());
+
+		}
+	}
+
+}
